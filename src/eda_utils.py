902851750cfd643ebd5679c2fdd3b9343ca63092
Lalise <lalisefufi5@gmail.com>
2026-05-23 11:56:50 +0300

    refactor: add reusable EDA utility functions

---
+++ src/eda_utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def calculate_loss_ratio(df):
+    return df['TotalClaims'].sum() / df['TotalPremium'].sum()
+
+
+def calculate_margin(df):
+    return df['TotalPremium'] - df['TotalClaims']
