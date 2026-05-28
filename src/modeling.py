58cdaa97e72e82bf5d17147d44fa57887361126e
Lalise <lalisefufi5@gmail.com>
2026-05-28 13:41:53 +0300

    Fix whitespace formatting in modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -3,9 +3,8 @@ import numpy as np
 
 
 def evaluate_regression(y_true, predictions):
-    
     rmse = np.sqrt(mean_squared_error(y_true, predictions))
-    
+
     r2 = r2_score(y_true, predictions)
-    
+
     return rmse, r2
